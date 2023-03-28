b26077ab8b797920b65c5b02ede971ee2a43e651
EliottPeres <eliott.peres@student.junia.com>
2023-03-28 23:19:34 +0200

    partie 3 et 4

--- calculatrice.js
+++ calculatrice.js
@@ -21,27 +21,36 @@
 
 class BaseCalculator {
   constructor() {
-
+    this.screen = document.getElementById('screen');
+    this.histo = [];
   }
   addToScreen(value) {
-    document.getElementById('screen').value += value;
+    this.screen.value += value;
   }
-  
+
   clearScreen() {
-    document.getElementById('screen').value = '';
+    this.screen.value = '';
   }
-  
+
   clearLast() {
-    let screen = document.getElementById('screen').value;
-    let taille = screen.length;
-    let clear = screen.substring(0, taille - 1);
-    document.getElementById('screen').value = clear;
+    let screen_ = this.screen.value;
+    let taille = screen_.length;
+    let clear = screen_.substring(0, taille - 1);
+    this.screen.value = clear;
   }
-  
+
   calculate() {
-    let input = document.getElementById('screen').value;
+    this.histo.push(this.screen.value);
+    let input = this.screen.value;
     let result = eval(input);
-    document.getElementById('screen').value = result;
+    this.screen.value = result;
+  }
+  historique() {
+    let hist=this.histo.pop();
+    if (hist != undefined) {
+      this.clearScreen();
+      this.addToScreen(hist);
+    }
   }
 }
 let baseCalculator = new BaseCalculator();
