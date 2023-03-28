ec363c93a5f8a0af2182f1b0cdd18c5d80b1acc4
EliottPeres <eliott.peres@student.junia.com>
2023-03-28 22:19:19 +0200

    test

--- calculatrice.js
+++ calculatrice.js
@@ -1,20 +1,47 @@
-function addToScreen(value) {
-  document.getElementById('screen').value += value;
-}
+// function addToScreen(value) {
+//   document.getElementById('screen').value += value;
+// }
 
-function clearScreen() {
-  document.getElementById('screen').value = '';
-}
+// function clearScreen() {
+//   document.getElementById('screen').value = '';
+// }
 
-function clearLast() {
-  let screen=document.getElementById('screen').value;
-  let taille=screen.length;
-  let clear=screen.substring(0,taille-1);
-  document.getElementById('screen').value = clear;
-}
+// function clearLast() {
+//   let screen = document.getElementById('screen').value;
+//   let taille = screen.length;
+//   let clear = screen.substring(0, taille - 1);
+//   document.getElementById('screen').value = clear;
+// }
 
-function calculate() {
-  let input = document.getElementById('screen').value;
-  let result = eval(input);
-  document.getElementById('screen').value = result;
+// function calculate() {
+//   let input = document.getElementById('screen').value;
+//   let result = eval(input);
+//   document.getElementById('screen').value = result;
+// }
+
+class BaseCalculator {
+  constructor() {
+
+  }
+  addToScreen(value) {
+    document.getElementById('screen').value += value;
+  }
+  
+  clearScreen() {
+    document.getElementById('screen').value = '';
+  }
+  
+  clearLast() {
+    let screen = document.getElementById('screen').value;
+    let taille = screen.length;
+    let clear = screen.substring(0, taille - 1);
+    document.getElementById('screen').value = clear;
+  }
+  
+  calculate() {
+    let input = document.getElementById('screen').value;
+    let result = eval(input);
+    document.getElementById('screen').value = result;
+  }
 }
+let baseCalculator = new BaseCalculator();
